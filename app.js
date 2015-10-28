290caf01d32863057ebbe12ae62ea699ab7cd653
aggaby <aggaby.ryu@gmail.com>
2015-10-28 16:26:50 +0900

    2nd save

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.get('/', function(req,res) {
-  res.send('Hello world!!!');
-});
+//app.get('/', function(req,res) {
+//  res.send('Hello world!!!');
+//});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+console.log(__dirname);
 
 app.listen(3000, function() {
-  console.log('Server On');
+  console.log('Server Start!!');
 });
